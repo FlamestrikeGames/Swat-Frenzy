8be1746ffaef210148b69d1264d9e89bdcac0c04
jonchou <jonchou@ucdavis.edu>
2016-11-19 17:06:38 -0800

    UI: Update titleViewController buttons

--- Swat Frenzy/Colors.swift
+++ Swat Frenzy/Colors.swift
@@ -10,4 +10,5 @@ import UIKit
 
 struct Colors {
     static let blueColor = UIColor(colorLiteralRed: 60/255, green: 99/255, blue: 163/255, alpha: 1.0)
+    static let greenColor = UIColor(colorLiteralRed: 110/255, green: 192/255, blue: 132/255, alpha: 1.0)
 }
--- Swat Frenzy/TitleScreenViewController.swift
+++ Swat Frenzy/TitleScreenViewController.swift
@@ -10,12 +10,57 @@ import UIKit
 
 class TitleScreenViewController: UIViewController {
     
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var tutorialButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Colors.blueColor
+
+        let borderColor = Colors.greenColor.cgColor
+        let bordWidth: CGFloat = 4.0
+        let buttonInsets = UIEdgeInsetsMake(0, 10, 0, 10)
+        
+        playButton.layer.borderColor = borderColor
+        playButton.layer.borderWidth = bordWidth
+        playButton.layer.cornerRadius = 25.0
+        playButton.titleEdgeInsets = buttonInsets
+        playButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        
+        tutorialButton.layer.borderColor = borderColor
+        tutorialButton.layer.borderWidth = bordWidth
+        tutorialButton.layer.cornerRadius = 20.0
+        tutorialButton.titleEdgeInsets = buttonInsets
+        tutorialButton.titleLabel?.adjustsFontSizeToFitWidth = true
+
+        resetButton.layer.borderColor = borderColor
+        resetButton.layer.borderWidth = bordWidth
+        resetButton.layer.cornerRadius = 15.0
+        resetButton.titleEdgeInsets = buttonInsets
+        resetButton.titleLabel?.adjustsFontSizeToFitWidth = true
+
     }
 
+    @IBAction func displayTutorial(_ sender: Any) {
+        
+    }
+    
     @IBAction func resetGameProgress(_ sender: Any) {
+        let alert = UIAlertController(title: "Reset Progress", message: "Are you sure you want to reset?", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "No", style: .cancel,
+                                         handler: nil)
+        let yesAction = UIAlertAction(title: "Yes", style: .default, handler: {
+            action in
+            self.resetProgress()
+        })
+        
+        alert.addAction(cancelAction)
+        alert.addAction(yesAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func resetProgress() {
         let userDef = UserDefaults.standard
         userDef.set(0, forKey: "goldAmount")
         userDef.set(0, forKey: "bonusPower")
